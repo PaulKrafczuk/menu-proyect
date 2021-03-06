dec086b7662f16c0034e606387c08d9e9cd268c4
Paul Krafczuk <krafczukpaul@gmail.com>
2021-03-05 22:24:53 -0300

    filter buttons finished

--- app.js
+++ app.js
@@ -73,22 +73,48 @@ const menu = [{
 ];
 
 const sectionCenter = document.querySelector('.section-center');
+const filterBtns = document.querySelectorAll('.filter-btn');
 
+//load items
 window.addEventListener('DOMContentLoaded', function() {
-    let displayMenu = menu.map(function(item) {
+    displayMenuItems(menu);
+});
+
+//filter items
+filterBtns.forEach(function(btn) {
+    btn.addEventListener('click', function(e) {
+        const category = e.currentTarget.dataset.id;
+        const menuCategory = menu.filter(function(menuItem) {
+            if (menuItem.category === category) {
+                return menuItem;
+            };
+        });
+        // console.log('perro');
+        if (category === "all") {
+            displayMenuItems(menu);
+        } else {
+            displayMenuItems(menuCategory);
+        }
+        console.log(menuCategory);
+    });
+});
+
+
+function displayMenuItems(menuItems) {
+    let displayMenu = menuItems.map(function(item) {
 
         return ` <article class="menu-item">
-        <img src="${item.img}" class="photo" alt=${item.title}>
-        <div class="item-info">
-            <header>
-                <h4>${item.title}</h4>
-                <h4 class="price">$${item.price}</h4>
-            </header>
-            <p class="item-text">${item.desc}</p>
-        </div>
-    </article>`;
-    })
+    <img src="${item.img}" class="photo" alt=${item.title}>
+    <div class="item-info">
+        <header>
+            <h4>${item.title}</h4>
+            <h4 class="price">$${item.price}</h4>
+        </header>
+        <p class="item-text">${item.desc}</p>
+    </div>
+</article>`;
+    });
 
     displayMenu = displayMenu.join("");
     sectionCenter.innerHTML = displayMenu;
-});
+};
